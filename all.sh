503b1947ffeb6191e59ace79d9ae62f22a3ea33d
Keishi Sando <nobuta05@gmail.com>
2020-08-28 02:47:46 +0900

    minor change

--- all.sh
+++ all.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 SCRIPTDIR=$(cd $(dirname $0); pwd)
-
 source ${SCRIPTDIR}/common.sh
-_tests_reset
 
+_tests_reset
 for test_source in `ls ${SCRIPTDIR}/test_*.sh`; do
   source ${test_source}
 done
-
 tests_result
