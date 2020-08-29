2f6d78b1e353fc0559e13114baa68adaa5ef058a
Keishi Sando <nobuta05@gmail.com>
2020-08-30 00:45:33 +0900

    updated

--- test_huga.sh
+++ test_huga.sh
@@ -2,5 +2,5 @@
 
 assert_equals "echo 'huga'" "huga"
 assert_ok "cat ${SCRIPTDIR}/test_hoge.sh"
-assert_ng "ping -i a -c 5 127.0.0.1" 2 # これおかしい
+assert_ng "ping -i a -c 5 127.0.0.1" 2
 assert_ok "ping -i 1 -c 5 127.0.0.1"
